3f1d85fa791908d46f1ab80aeab5bb5464b5a2d9
HossamKhir <h.khair95@outlook.com>
2022-12-01 23:20:36 +0000

    feat: learned about simple naming conventions; and line, summary & multiline comments

--- Program.cs
+++ Program.cs
@@ -4,21 +4,42 @@ namespace spc_csharp
 {
   class Program
   {
+    /// <summary>
+    /// This is the entry point of the programme
+    /// </summary>
     static void Main(string[] args)
     {
-      // a declaration is a statement that tells the variable type
-      int declaration;
-      // the definition is a statement that declares the variable type, and set a value on it
-      int definition = 1;
+      // in C#, naming conventions follow camelCase
+      float productA = 3.99f; // `f` is for float
+      float productB = 11.99f;
+      float productC = 5.75f;
 
-      // NOTE: both definition & declaration are done once
-      // no variable would be redeclared or redefined
+      float total = productA + productB + productC;
 
-      declaration = definition + definition;
+      float vat = 19f;
 
-      Console.WriteLine("Result is " + declaration);
+      float totalTax = total * vat / 100;
 
-    //   Console.WriteLine("Hello world");
+      // C# also accepts shorthand assignment operators 
+      total += totalTax;
+
+      Console.WriteLine("Total price is " + total);
+
+      /* This is a multiline comment
+        // a declaration is a statement that tells the variable type
+        int declaration;
+        // the definition is a statement that declares the variable type, and set a value on it
+        int definition = 1;
+
+        // NOTE: both definition & declaration are done once
+        // no variable would be redeclared or redefined
+
+        declaration = definition + definition;
+
+        Console.WriteLine("Result is " + declaration);
+
+        Console.WriteLine("Hello world");
+        */
     }
   }
 }
