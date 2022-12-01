8feff863b2a248187439a0b47ac305f6606301a8
HossamKhir <h.khair95@outlook.com>
2022-12-01 22:48:11 +0000

    feat: created new console app

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace spc_csharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      string name = "ObiWan";
+      // concatenation
+      Console.WriteLine("Hello, " + name);
+      // we can have arithmatic operations
+      Console.WriteLine(2 + 2 * 6);
+
+      // there are several types of variables, we seen string above ^
+      int number = 2;
+      float price = 3.99f;
+
+      Console.WriteLine(number * price);
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Workspace for C\#
+
+## Getting started
+
+To start a simple console app:
+
+```sh
+dotnet new console
+```
+
+> use `dotnet new --list` to get a list of available templates
