866a2325747597bbf57de9035bf144309196077d
Hossam Khair <hossam.khir1@outlook.com>
2022-12-02 16:57:17 +0200

    feat: introduction about variable scope

--- Program.cs
+++ Program.cs
@@ -2,41 +2,55 @@
 
 class Program
 {
-  /// <summary>
-  /// This is the entry point of the programme
-  /// </summary>
-  static void Main(string[] args)
-  {
-    // in C#, naming conventions follow camelCase
-    float productA = 3.99f; // `f` is for float
-    float productB = 11.99f;
-    float productC = 5.75f;
+    // static float vat = 19f; // this is a static class variable (attribute)
 
-    float total = productA + productB + productC;
+    /// <summary>
+    /// This is the entry point of the programme
+    /// </summary>
+    static void Main(string[] args) // this is called a method
+    { // the scope of the Main method starts after this open brace, ends w/ a closing one
+      // in C#, naming conventions follow camelCase
+        float productA = 3.99f; // `f` is for float
+        float productB = 11.99f;
+        float productC = 5.75f;
 
-    float vat = 19f;
+        float total = productA + productB + productC;
 
-    float totalTax = total * vat / 100;
+        // if the class variable is uncommented, this will overshadow its value,
+        // then vat will be 15 instead of 19
+        float vat = 15f;
 
-    // C# also accepts shorthand assignment operators 
-    total += totalTax;
+        float totalTax = total * vat / 100;
 
-    Console.WriteLine("Total price is " + total);
+        // C# also accepts shorthand assignment operators 
+        total += totalTax;
 
-    /* This is a multiline comment
-      // a declaration is a statement that tells the variable type
-      int declaration;
-      // the definition is a statement that declares the variable type, and set a value on it
-      int definition = 1;
+        Console.WriteLine("Total price is " + total);
 
-      // NOTE: both definition & declaration are done once
-      // no variable would be redeclared or redefined
+        /* This is a multiline comment
+          // a declaration is a statement that tells the variable type
+          int declaration;
+          // the definition is a statement that declares the variable type, and set a value on it
+          int definition = 1;
 
-      declaration = definition + definition;
+          // NOTE: both definition & declaration are done once
+          // no variable would be redeclared or redefined
 
-      Console.WriteLine("Result is " + declaration);
+          declaration = definition + definition;
 
-      Console.WriteLine("Hello world");
-      */
-  }
+          Console.WriteLine("Result is " + declaration);
+
+          Console.WriteLine("Hello world");
+          */
+    }
+
+    static void calculateVat()
+    {
+        // System.Console.WriteLine("Vat is " + vat);
+        /**
+        uncommenting the above line, while the class variable is commented out
+        will cause a compilation error, as `vat` is not in the method's scope,
+        or the class scope
+        */
+    }
 }
