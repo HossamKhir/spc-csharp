91fb4f043b9ce9abd01c8e045cdde670733b97db
HossamKhir <h.khair95@outlook.com>
2022-12-02 00:00:46 +0000

    refactor: updated .gitignore; updated .net sdk

--- Program.cs
+++ Program.cs
@@ -1,45 +1,42 @@
 ﻿using System;
 
-namespace spc_csharp
+class Program
 {
-  class Program
+  /// <summary>
+  /// This is the entry point of the programme
+  /// </summary>
+  static void Main(string[] args)
   {
-    /// <summary>
-    /// This is the entry point of the programme
-    /// </summary>
-    static void Main(string[] args)
-    {
-      // in C#, naming conventions follow camelCase
-      float productA = 3.99f; // `f` is for float
-      float productB = 11.99f;
-      float productC = 5.75f;
+    // in C#, naming conventions follow camelCase
+    float productA = 3.99f; // `f` is for float
+    float productB = 11.99f;
+    float productC = 5.75f;
 
-      float total = productA + productB + productC;
+    float total = productA + productB + productC;
 
-      float vat = 19f;
+    float vat = 19f;
 
-      float totalTax = total * vat / 100;
+    float totalTax = total * vat / 100;
 
-      // C# also accepts shorthand assignment operators 
-      total += totalTax;
+    // C# also accepts shorthand assignment operators 
+    total += totalTax;
 
-      Console.WriteLine("Total price is " + total);
+    Console.WriteLine("Total price is " + total);
 
-      /* This is a multiline comment
-        // a declaration is a statement that tells the variable type
-        int declaration;
-        // the definition is a statement that declares the variable type, and set a value on it
-        int definition = 1;
+    /* This is a multiline comment
+      // a declaration is a statement that tells the variable type
+      int declaration;
+      // the definition is a statement that declares the variable type, and set a value on it
+      int definition = 1;
 
-        // NOTE: both definition & declaration are done once
-        // no variable would be redeclared or redefined
+      // NOTE: both definition & declaration are done once
+      // no variable would be redeclared or redefined
 
-        declaration = definition + definition;
+      declaration = definition + definition;
 
-        Console.WriteLine("Result is " + declaration);
+      Console.WriteLine("Result is " + declaration);
 
-        Console.WriteLine("Hello world");
-        */
-    }
+      Console.WriteLine("Hello world");
+      */
   }
 }
