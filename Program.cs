345a94af8bc0f3dbf9986cd9797cbbe3ce1e1970
Hossam Khair <hossam.khir1@outlook.com>
2022-12-09 19:43:05 +0200

    feat: read user input & convert/parse it

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ class Program
     {
         // See https://aka.ms/new-console-template for more information
         // Console.WriteLine("Hello, World!");
-        string input = Console.ReadLine();
-        Console.WriteLine(input);
+        Console.WriteLine("Please enter a number");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter another number");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"result is\t:\t{num1 + num2}");
+        // string input = Console.ReadLine();
+        // Console.WriteLine(input);
     }
 }
