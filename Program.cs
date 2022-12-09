43f1337f6ff13a1ba93beee128c5e2cb4f10af32
Hossam Khair <hossam.khir1@outlook.com>
2022-12-09 19:37:48 +0200

    feat: how to read input from command line

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        // Console.WriteLine("Hello, World!");
+        string input = Console.ReadLine();
+        Console.WriteLine(input);
+    }
+}
