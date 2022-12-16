9cfb0a490260268aeafb7d23e6f3639f5d09cb13
Hossam Khair <hossam.khir1@outlook.com>
2022-12-16 18:19:44 +0200

    feat: learned about for loops

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+public static class Program
+{
+    private const int DATUM = 10;
+    private const int STEP = 1;
+    public static void Main(string[] args)
+    {
+        // INFO: general syntax
+        for (int counter = 0; counter < DATUM; counter += STEP)
+        {
+            Console.WriteLine(counter);
+        }
+        PrintOddNumber();
+    }
+
+    public static void PrintOddNumber()
+    {
+        for (int index = 1; index < DATUM; index += 2)
+        {
+            Console.WriteLine(index);
+        }
+    }
+}
