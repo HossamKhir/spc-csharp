f4a1c32bf486c5335333d7b5002d99094217eaca
Hossam Khair <hossam.khir1@outlook.com>
2022-12-16 18:28:25 +0200

    feat: learned about do-while loops

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int counter = 0;
+// INFO: general syntax
+do
+{
+    System.Console.WriteLine(counter++);
+} while (counter < 10);
+// NOTE: the loop is guaranteed to run at least once
