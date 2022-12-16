ffe79617ba9cbddc0a1264906a9f82a90f008d0b
Hossam Khair <hossam.khir1@outlook.com>
2022-12-16 23:17:29 +0200

    feat: learned more about constructors

--- Human.cs
+++ Human.cs
@@ -6,9 +6,16 @@ namespace Classes
     {
         private string _forename;
         private string _surname;
+        private string _eyeColour;
+        private int _age;
 
         // INFO: by default, each class would have an empty default constructor
-        //  unless at one new constructor is defined
+        //  unless at one new (optionally parametrised) constructor is defined
+
+        public Human()
+        {
+            // INFO: default empty constructor
+        }
 
         // INFO: constructors have no return type, but they still do have access modifiers
         public Human(string forename, string surname)
@@ -16,10 +23,17 @@ namespace Classes
             this._forename = forename;
             this._surname = surname;
         }
+        // INFO: constructors can be overloaded
+        public Human(string forename, string surname, string eyeColour, int age)
+        : this(forename, surname) // NOTE: you may want to use other constructors, this is the syntax to how to do that
+        {
+            this._eyeColour = eyeColour;
+            this._age = age;
+        }
 
         public void IntroduceYourself()
         {
-            System.Console.WriteLine($"My name is {_surname}, {_forename} {_surname}!");
+            Console.WriteLine($"My name is {_surname}, {_forename} {_surname}!");
         }
     }
 }
