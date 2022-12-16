070262b4f105026363a47709ea0c635fed8e908e
Hossam Khair <hossam.khir1@outlook.com>
2022-12-16 20:22:37 +0200

    feat: learned about while loops

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int counter = 0;
+// INFO: general syntax
+while (counter < 10)
+{
+    Console.WriteLine(counter);
+    counter++;
+}
