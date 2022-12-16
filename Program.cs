fce90208fbf34ba5cf4fcea896ed4096a0147da3
Hossam Khair <hossam.khir1@outlook.com>
2022-12-16 20:29:10 +0200

    feat: learned about break & continue in loops

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for (int index = 0; index < 10; ++index)
+{
+    if (index == 3)
+    {
+        System.Console.WriteLine("No 3 for you!");
+        continue; // skips the current iteration, and starts the next iteration
+    }
+    if (index == 6)
+    {
+        Console.WriteLine("You shall not pass!");
+        break; // exits from (breaks) the loop, and go outside it
+    }
+    Console.WriteLine(index);
+}
+Console.WriteLine("That's it, folks!");
