609c1398fda93d46ba148b7a037e5c27ce9004fa
Hossam Khair <hossam.khir1@outlook.com>
2022-12-16 23:52:13 +0200

    feat: learned about properties

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+namespace Properties
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Box box = new Box();
+            // box.Length = 5.0;
+            // box.Width = 1.0;
+            // box.Height = 1;
+            Box box = new Box(5, 1, 1);
+            Console.WriteLine(box.Volume);
+        }
+    }
+}
